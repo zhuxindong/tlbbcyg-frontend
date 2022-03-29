8f48952cb38d5e5032df513feda477a08c8aa4b1
zhuxindong <501801307@qq.com>
2022-03-29 15:45:32 +0800

    看懂了一些

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+    <div class="dashboard-text">
+
+      <!-- name: {{ name }} -->
+      简单说明：
+      可以点击左侧的按条件查询
+
+    </div>
   </div>
 </template>
 
@@ -8,7 +14,7 @@
 import { mapGetters } from 'vuex'
 
 export default {
-  name: 'Dashboard',
+  name: '天龙八部畅易阁助手',
   computed: {
     ...mapGetters([
       'name'
